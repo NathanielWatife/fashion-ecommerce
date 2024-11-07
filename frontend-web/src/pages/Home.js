f339e43a20d395fe0fcdca29a47c58daf668b33f
NathanielWatife <awosanminathaniel0@gmail.com>
2024-11-07 23:39:47 +0100

    Redux fetching product from api

--- frontend-web/src/pages/Home.js
+++ frontend-web/src/pages/Home.js
@@ -1,3 +1,31 @@
-import React from "react";
-const Home = () => <h1>Welcome to Fashion E-commerce Home Page</h1>;
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchProducts } from '../redux/productSlice';
+import ProductCard  from "../components/ProductCard";
+
+
+const Home = () => {
+    const dispatch = useDispatch();
+    const products = useSelector((state) => state.products.items);
+    const loading = useSelector((state) => state.products.loading);
+
+    useEffect(() => {
+        dispatch(fetchProducts());
+    }, [displatch]);
+    if (loading){
+        return <p>Loading...</p>
+    }
+
+    return (
+        <div className="home-page">
+            <h2>Our Products</h2>
+            <div className="product-grid">
+                {products.map((product) => (
+                    <ProductCard key={product.id} product={product} />
+                ))}
+            </div>
+        </div>
+    );
+};
+
 export default Home;
